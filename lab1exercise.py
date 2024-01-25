4629f36c255ce70b806f1f86dd85fc031e2dd015
Kemelbek <k_kavu@kbtu.kz>
2024-01-25 23:46:33 +0600

    kemelbek

---
+++ lab1exercise.py
@@ -0,0 +1,71 @@
+# Python Syntax
+# print("Hello World")
+
+#if 5 > 2:
+    #print("YES")
+    
+#Python Comments
+#This is a comment
+"""
+This is a comment
+written in 
+more than just one line
+"""
+
+#Python Variables
+#carname = "Volvo"
+#x=50
+
+#x=5
+#y=10
+#print(x+y)
+
+#x=5
+#y=10
+#z=x+y
+#print(z)
+#x,y,z = "Orange", "Banana", "Cherry"
+#x=y=z="Orange"
+#def  myfunc():
+#global x
+#x="fantastic"
+
+#Python data types
+#x=5
+#print(int(x))
+#x = "Hello World"
+#print(str(x))
+#x = 20.5
+#print(float(x))
+#x = ["apple", "banana", "cherry"]
+#print(list(x))
+#x = ("apple", "banana", "cherry")
+#print(tuple(x))
+#x = {"name" : "John", "age" : 36}
+#print(dict(x))
+#x = True
+#print(bool(x))
+
+#Python numbers
+#x = 5
+#x = float(x)
+#x=5.5
+#x=int(x)
+#x=5
+#x=complex(x)
+
+#Python string
+#x = "Hello World"
+#print(len(x))
+#txt = "Hello World"
+#x=txt[0]
+#txt = "Hello World"
+#x=txt[2:5]
+#txt = " Hello World "
+#x=txt.strip()
+#txt = "Hello World"
+#txt=txt.upper()
+#txt = "Hello World"
+#txt=txt.lower()
+#txt = "Hello World"
+#txt=txt.replace("H", "J")
